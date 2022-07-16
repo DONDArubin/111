c2c564d7fc4ecfd16e53e21dec2ce1b91ba35392
DONDArubin <102237672+DONDArubin@users.noreply.github.com>
2022-07-16 21:34:10 +0400

    Add files via upload

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,207 +1,72 @@
 import './Map.css';
+import {React, useState} from "react";
+import dataMap from "../../data/Map.json";
+import Modal from 'react-bootstrap/Modal';
+import Carousel from 'react-bootstrap/Carousel';
+
+
 
 const Map = () => {
+    const data = dataMap.Map;
+
+    const [show, setShow] = useState(false);
+    const [modalId, setModalId] = useState(null);
+    
+
+    const activateModal = (id) => {
+        setShow(true);
+        setModalId(id);
+    };
+
+    const cards = data.map((item) => {
+        return (
+            <div className="place__card" id={item.id} key={item.id} onClick={() => activateModal(item.id)}>
+                <div className="image__block">
+                    <img className="image" src={item.image} alt=""/>
+                </div>
+                <h2 className="title">{item.title}</h2>
+            </div>
+            )
+    });
+
+    function createModal(id, mapData = data) {
+        const currentItem = mapData[id - 1];
+
+        return (
+            <Modal show={show} onHide={() => setShow(false)} size={"lg"} centered>
+                <Modal.Header closeButton>
+                <Modal.Title>{currentItem.title}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Carousel>
+                        {
+                            currentItem.gallery?.map((el) => {
+                                return (
+                                    <Carousel.Item key={el}>
+                                        <img className="modal-image" src={el} alt={currentItem.type} />
+                                    </Carousel.Item>
+                                )
+                            })
+                        }
+                    </Carousel>
+                    <div className='info'>
+                        <div className="address">{currentItem.address}</div>
+                        <div className="marks">{currentItem.marks}</div>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <div className="type">{currentItem.type}</div>
+                </Modal.Footer>
+            </Modal>
+        )
+    }
+
+    const modal = show ? createModal(modalId) : null;
+
     return (
         <div className={"cards"}>
-
-
-            <div className={"place__card"}>
-
-                <div className={"image__block"}>
-                    <img className={"image"} src={"https://lh5.googleusercontent.com/p/AF1QipOafNWlUb9zNkt5ZtSjbIjqukipNxlBxd4j1vTm=w408-h272-k-no"} alt={""}/>
-                </div>
-
-                <div className={"info"}>
-
-                    <div className={"title"}>
-                        <h2>KFC</h2>
-                    </div>
-
-                    <div className={"address"}>
-                        <p>2-я Садовая ул., д.106</p>
-                    </div>
-
-                    <div className={"marks"}>
-                        <span>* * * * *</span>
-                    </div>
-
-                    <div className={"type"}>
-                        <p>Фастфуд</p>
-                    </div>
-                </div>
-
-            </div>
-
-
-            <div className={"place__card"}>
-
-                <div className={"image__block"}>
-                    <img className={"image"} src={"https://kinf.ru/image/w/fn/udg5fs1wjffigeya.jpg"} alt={""}/>
-                </div>
-
-                <div className={"info"}>
-
-                    <div className={"title"}>
-                        <h2>Pro Hook</h2>
-                    </div>
-
-                    <div className={"address"}>
-                        <p>2-я Садовая ул., д.99</p>
-                    </div>
-
-                    <div className={"marks"}>
-                        <span>* * * * *</span>
-                    </div>
-
-                    <div className={"type"}>
-                        <p>Vape Shop</p>
-                    </div>
-                </div>
-
-            </div>
-
-
-            <div className={"place__card"}>
-
-                <div className={"image__block"}>
-                    <img className={"image"} src={"https://avatars.mds.yandex.net/get-altay/4304228/2a0000017759f53d9e0ee9fcbc01a16aebb7/XXL"} alt={""}/>
-                </div>
-
-                <div className={"info"}>
-
-                    <div className={"title"}>
-                        <h2>Ice Hook</h2>
-                    </div>
-
-                    <div className={"address"}>
-                        <p>Беговая ул., д.19</p>
-                    </div>
-
-                    <div className={"marks"}>
-                        <span>* * * * *</span>
-                    </div>
-
-                    <div className={"type"}>
-                        <p>Vape Shop</p>
-                    </div>
-                </div>
-
-            </div>
-
-
-            <div className={"place__card"}>
-
-                <div className={"image__block"}>
-                    <img className={"image"} src={"https://lh5.googleusercontent.com/p/AF1QipMypFY7IXT1WncxHrcir43bUR7Sqpj91WYDQchG=w485-h240-k-no"} alt={""}/>
-                </div>
-
-                <div className={"info"}>
-
-                    <div className={"title"}>
-                        <h2>Научно-техническая библиотека СГТУ</h2>
-                    </div>
-
-                    <div className={"address"}>
-                        <p>Политехническая ул., 77, корп. 25</p>
-                    </div>
-
-                    <div className={"marks"}>
-                        <span>* * * * *</span>
-                    </div>
-
-                    <div className={"type"}>
-                        <p>Библиотека</p>
-                    </div>
-                </div>
-
-            </div>
-
-
-            <div className={"place__card"}>
-
-                <div className={"image__block"}>
-                    <img className={"image"} src={"https://avatars.mds.yandex.net/get-altay/6057477/2a000001815f2d63d05b771ead8000968cca/XXL"} alt={""}/>
-                </div>
-
-                <div className={"info"}>
-
-                    <div className={"title"}>
-                        <h2>Стадион СГТУ</h2>
-                    </div>
-
-                    <div className={"address"}>
-                        <p>Политехническая ул., 77, корп. 3</p>
-                    </div>
-
-                    <div className={"marks"}>
-                        <span>* * * * *</span>
-                    </div>
-
-                    <div className={"type"}>
-                        <p>Спорт</p>
-                    </div>
-                </div>
-
-            </div>
-
-
-            <div className={"place__card"}>
-
-                <div className={"image__block"}>
-                    <img className={"image"} src={"https://lh5.googleusercontent.com/p/AF1QipO6o2isZo8TskvNHGUUY5IjjpcqOsGPuQGIh0Pr=w408-h240-k-no-pi-0-ya80-ro-0-fo100"} alt={""}/>
-                </div>
-
-                <div className={"info"}>
-
-                    <div className={"title"}>
-                        <h2>Детский <br/> технопарк <br/> «Кванториум»</h2>
-                    </div>
-
-                    <div className={"address"}>
-                        <p>Политехническая ул., 77, корп. 25</p>
-                    </div>
-
-                    <div className={"marks"}>
-                        <span>* * * * *</span>
-                    </div>
-
-                    <div className={"type"}>
-                        <p>Наука</p>
-                    </div>
-                </div>
-
-            </div>
-
-
-            <div className={"place__card"}>
-
-                <div className={"image__block"}>
-                    <img className={"image"} src={"https://sun7-7.userapi.com/s/v1/ig2/QF3cANfWNspA9cfNB3BNixQL8zRM-X0NEUoGmxmGXeDAGLAy2hxxXrYeOe9atqdCRiGmQbZjFOQDZP6lgAtvynLI.jpg?size=400x400&quality=96&crop=0,0,600,600&ava=1"} alt={""}/>
-                </div>
-
-                <div className={"info"}>
-
-                    <div className={"title"}>
-                        <h2>IMBA</h2>
-                    </div>
-
-                    <div className={"address"}>
-                        <p>2-я Садовая ул., 106Бк6</p>
-                    </div>
-
-                    <div className={"marks"}>
-                        <span>* * * * *</span>
-                    </div>
-
-                    <div className={"type"}>
-                        <p>Компьютерный клуб</p>
-                    </div>
-                </div>
-
-            </div>
-
-
-
+            {cards}
+            {modal}
         </div>
     )
 }
